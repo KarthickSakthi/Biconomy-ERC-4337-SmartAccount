552fbb393fd41c4c5a60cf11221d3deb4ab41286
Karthick S <karthicksakthi124@gmail.com>
2023-05-16 15:45:30 +0000

    comment added

--- biconomy-dapp-erc4337/pages/_app.js
+++ biconomy-dapp-erc4337/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import "@biconomy/web3-auth/dist/src/style.css";
+import "@biconomy/web3-auth/dist/src/style.css"; // importing styles for biconomy component
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
